edf95edbceab36503f3f094826aea9550ada8014
James Wilson <wilsjame@oregonstate.edu>
2017-04-12 19:02:02 -0700

    finished Q5

--- a1/Q5.c
+++ a1/Q5.c
@@ -1,6 +1,6 @@
 /* CS261- Assignment 1 - Q.5*/
-/* Name:
- * Date:
+/* Name: James Wilson
+ * Date: 12 April 2017
  * Solution description:
  */
  
@@ -19,14 +19,42 @@ char toLowerCase(char ch){
 
 void sticky(char* word){
      /*Convert to sticky caps*/
+	int index = 0;
+
+	while(word[index] != '\0'){
+		if(index % 2 == 0){
+			if(word[index] >= 'A' && word[index] <= 'Z'){
+				/*do nothing*/
+			}
+			else{
+				word[index] = toUpperCase(word[index]);
+			}
+		}
+		else{
+			if(word[index] >= 'A' && word[index] <= 'Z'){
+				word[index] = toLowerCase(word[index]);
+			}
+			else{
+				/*do nothing*/
+			}
+		}
+
+		index++;
+	}
+
 }
 
 int main(){
     /*Read word from the keyboard using scanf*/
+	char user_input[30];
+	printf("Enter a word to modify with sticky caps: ");
+	scanf("%s", user_input);
     
     /*Call sticky*/
+	sticky(user_input);
     
     /*Print the new word*/
+	printf("%s\n", user_input);
     
     return 0;
 }
