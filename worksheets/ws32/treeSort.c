6801ba84e761bddcefbcff925cb2a5e288901448
James Wilson <wilsjame@oregonstate.edu>
2017-06-06 19:44:29 -0700

    finish worksheet treeSort

--- worksheets/ws32/treeSort.c
+++ worksheets/ws32/treeSort.c
@@ -3,18 +3,40 @@
 #endif
 
 void treeSort(TYPE *data, int n){ /* sort values in array data */
-	AVLtree tree;
-	AVLtreeInit(&tree);
-
 	//1. copy values into tree, O(nlogn)
 	//2. copy them out (into data[]) using in-order traversal, O(n)
+assert(data != NULL);
+
+	struct AVLTree tree;
+
+	initAVLTree(&tree);
+
+	for (int i = 0; i < n; i++)
+
+	{
+
+		addAVLTree(&tree, data[i]);
+
+	}
 
 
 
+	struct AVLIterator itr;
+
+	AVLIteratorInit(&tree, &itr);
 
 
 
+	int index = 0;
 
+	while (AVLIteratorHasNext(&itr))
 
+	{
+
+		data[index] = AVLIteratorNext(&itr);
+
+		index++;
+
+	}
 	
 }
